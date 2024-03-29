539bc13b2abb49f3e9f3aed3bda6ed3a785fd138
Huy Pham <quochuypham1505@gmail.com>
2024-03-29 13:03:23 +0700

    Update ThingsboardAppConstants

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,7 +1,12 @@
 abstract class ThingsboardAppConstants {
-  static final thingsBoardApiEndpoint = 'http://localhost:8080';
+  static final thingsBoardApiEndpoint = 'https://smart.dienquang.com';
+  // static final thingsBoardApiEndpoint = 'https://demo.thingsboard.io';
+  // static final thingsBoardApiEndpoint = 'http://172.16.23.52:8080';
+
   static final thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
+  // static final thingsboardOAuth2CallbackUrlScheme =
+  //     'https://smart.dienquang.com/login/oauth2/code/';
 
   /// Not for production (only for debugging)
-  static final thingsboardOAuth2AppSecret = 'Your app secret here';
+  static final thingsboardOAuth2AppSecret = 'htDOAjVYQCOqUYTEqq5H2HKA';
 }
